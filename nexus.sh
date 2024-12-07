08a1de5accb4e4c819a1a0cf4e1d4a555d9e46f1
0xGareeb <117015135+arun993@users.noreply.github.com>
2024-12-07 06:45:12 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -2,6 +2,10 @@
 
 rustc --version || curl https://sh.rustup.rs -sSf | sh
 NEXUS_HOME=$HOME/.nexus
+GREEN='\033[1;32m'
+ORANGE='\033[1;33m'
+NC='\033[0m' # No Color
+mkdir -p $HOME/.nexus
 
 while [ -z "$NONINTERACTIVE" ] && [ ! -f "$NEXUS_HOME/prover-id" ]; do
     read -p "Do you agree to the Nexus Beta Terms of Use (https://nexus.xyz/terms-of-use)? (Y/n) " yn </dev/tty
@@ -20,33 +24,26 @@ if [ $GIT_IS_AVAILABLE != 0 ]; then
   exit 1;
 fi
 
-PROVER_ID=$(cat "$NEXUS_HOME/prover-id" 2>/dev/null)
-
-if [ -z "$NONINTERACTIVE" ] && [ "${#PROVER_ID}" -ne 28 ]; then
-    echo "To receive credit for proving in Nexus testnets, click on your prover ID"
-    echo "(bottom left) at https://beta.nexus.xyz/ to copy the full Prover ID and"
-    echo "paste it here. Press Enter to continue."
-    
-    while true; do
-        read -p "Prover ID (optional)> " PROVER_ID </dev/tty
-        if [ -z "$PROVER_ID" ]; then
-            echo "Prover ID is empty. Skipping..."
-            break
-        fi
-
-        if [ ${#PROVER_ID} -eq 28 ]; then
-            # Backup existing Prover ID if it exists
+PROVER_ID=$(cat $NEXUS_HOME/prover-id 2>/dev/null)
+if [ -z "$NONINTERACTIVE" ] && [ "${#PROVER_ID}" -ne "28" ]; then
+    echo "\nTo receive credit for proving in Nexus testnets..."
+    echo "\t1. Go to ${GREEN}https://beta.nexus.xyz${NC}"
+    echo "\t2. On the bottom left hand corner, copy the ${ORANGE}prover id${NC}"
+    echo "\t3. Paste the ${ORANGE}prover id${NC} here. Press Enter to continue.\n"
+    read -p "Enter your Prover Id (optional)> " PROVER_ID </dev/tty
+    while [ ! ${#PROVER_ID} -eq "0" ]; do
+        if [ ${#PROVER_ID} -eq "28" ]; then
             if [ -f "$NEXUS_HOME/prover-id" ]; then
-                echo "Copying $NEXUS_HOME/prover-id to $NEXUS_HOME/prover-id.bak"
-                cp "$NEXUS_HOME/prover-id" "$NEXUS_HOME/prover-id.bak"
+                echo Copying $NEXUS_HOME/prover-id to $NEXUS_HOME/prover-id.bak
+                cp $NEXUS_HOME/prover-id $NEXUS_HOME/prover-id.bak
             fi
-            # Save the new Prover ID
-            echo "$PROVER_ID" > "$NEXUS_HOME/prover-id"
-            echo "Prover ID saved to $NEXUS_HOME/prover-id."
-            break
+            echo "$PROVER_ID" > $NEXUS_HOME/prover-id
+            echo Prover id saved to $NEXUS_HOME/prover-id.
+            break;
         else
-            echo "Invalid Prover ID. Please ensure it is exactly 28 characters."
+            echo Unable to validate $PROVER_ID. Please make sure the full prover id is copied.
         fi
+        read -p "Prover Id (optional)> " PROVER_ID </dev/tty
     done
 fi
 
@@ -55,10 +52,8 @@ if [ -d "$REPO_PATH" ]; then
   echo "$REPO_PATH exists. Updating.";
   (cd $REPO_PATH && git stash save && git fetch --tags)
 else
-  mkdir -p $NEXUS_HOME
   (cd $NEXUS_HOME && git clone https://github.com/nexus-xyz/network-api)
 fi
 (cd $REPO_PATH && git -c advice.detachedHead=false checkout $(git rev-list --tags --max-count=1))
 
 (cd $REPO_PATH/clients/cli && cargo run --release --bin prover -- beta.orchestrator.nexus.xyz)
-
