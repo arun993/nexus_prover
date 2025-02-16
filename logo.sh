74eaf4b6d4d1c516b7d5bd64cc9d93cc382621e9
0xArun <117015135+arun993@users.noreply.github.com>
2025-02-16 11:05:08 +0530

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Define color and style variables
+
 RED='\033[0;31m'
 BOLD='\033[1m'
 RESET='\033[0m'
 
-# Function to center text in the terminal
+
 center_text() {
   local text="$1"
   local term_width
@@ -14,7 +14,7 @@ center_text() {
   printf "%*s%s\n" "$padding" "" "$text"
 }
 
-# Print ASCII art in red
+
 echo -e "${RED}"
 echo "███╗   ██╗███████╗██╗  ██╗██╗   ██╗███████╗     ██████╗██╗     ██╗"
 echo "████╗  ██║██╔════╝╚██╗██╔╝██║   ██║██╔════╝    ██╔════╝██║     ██║"
@@ -24,7 +24,7 @@ echo "██║ ╚████║███████╗██╔╝ ██╗
 echo "╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝╚══════╝╚═╝"
 echo -e "${RESET}"
 
-# Print additional centered text with red bold styling
+
 echo -e "${RED}${BOLD}"
 echo "Follow me on X/Twitter : https://x.com/Arun__993 "
 echo "Github : https://github.com/arun993 "
